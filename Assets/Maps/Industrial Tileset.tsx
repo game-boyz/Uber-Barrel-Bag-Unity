069e250dddc47bd6b60a74f2c79278e13027a436
Tony Powell <adpowell@mtu.edu>
2019-01-08 17:57:17 -0500

    Create tilemap, tilesets, debug level layout
    Added and configured tiles, tilsmaps, and tilesets.
    Collision should be added to the 'Platforms' tilemap and the 'Player'.

---
+++ Assets/Maps/Industrial Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Industrial Tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../../Dropbox/Game Development/Tilesets/0x72/16x16 Industrial Tileset/industrial.v2.png" width="512" height="512"/>
+</tileset>
