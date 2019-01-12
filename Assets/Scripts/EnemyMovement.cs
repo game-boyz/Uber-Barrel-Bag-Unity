cd6130268e7be57ee8bdef47dc6d086e5743cab4
Anthony Powell <adpowell@mtu.edu>
2019-01-12 12:11:40 -0500

    Setup enemy asset and animations
    Also implemented physics; He's got low grav since he's a floaty boy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using PhysicsObjects;
+
+public class EnemyMovement : PhysicsObject
+{
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
