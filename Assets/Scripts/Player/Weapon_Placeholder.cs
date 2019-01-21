661bb4b98e71ed9207541fe54237c9aeb615258d
Anthony Powell <adpowell@mtu.edu>
2019-01-20 22:53:03 -0500

    Remove current player weapon on new weapon pickup

--- Assets/Scripts/Player/Weapon_Placeholder.cs
+++ Assets/Scripts/Player/Weapon_Placeholder.cs
@@ -24,7 +24,13 @@ public class Weapon_Placeholder : MonoBehaviour
 
     public bool SpawnWeapon(int type) {
         if (type != current_weapon_type) {
-            Debug.Log($"Spawning {type}");
+
+            // Delete current player weapon (and all other children of Weapon_Placeholder
+            foreach (Transform child in transform) {
+                GameObject.Destroy(child.gameObject);
+            }
+
+            // Save the new weapon type and then create an instance of the weapon as a child of Weapon_Placeholder
             current_weapon_type = type;
             Instantiate(possible_weapons[type], transform.position, transform.rotation).transform.parent = gameObject.transform;
 
