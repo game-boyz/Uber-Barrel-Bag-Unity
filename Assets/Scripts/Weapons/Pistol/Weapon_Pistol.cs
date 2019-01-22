1dbcd8984ea6a7b9dad9cb8be56ed88ecf1ec5df
Connor <dalek124@gmail.com>
2019-01-22 18:47:29 -0500

    made better
    Streamlined organization and weapon script.

--- Assets/Scripts/Weapons/Pistol/Weapon_Pistol.cs
+++ Assets/Scripts/Weapons/Pistol/Weapon_Pistol.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using Weapons;
 
 public class Weapon_Pistol : Weapon {
-    public Animator animator;
+    
 
     private void Awake() {
-        animator = GetComponent<Animator>();
         type = (int)Weapon_Constants.Weapon_types.Pistol;
+        animator = GetComponent<Animator>();
     }
 
     protected override void Fire() {
-        animator.SetTrigger("Fire");
+        
     }
 }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -12,10 +12,12 @@ namespace Weapons {
         // A weapon class inheriting from this class must specify its type
         // Types can be retrieved from Weapon_Constant.Weapon_Types in the Weapons namespace
         public int type;
+        public Animator animator;
 
         void Update() {
             // Fires when f key or left click is pressed
             if (Input.GetButtonDown("Fire1")) {
+                animator.SetTrigger("Fire");
                 Fire();
             }
         }
