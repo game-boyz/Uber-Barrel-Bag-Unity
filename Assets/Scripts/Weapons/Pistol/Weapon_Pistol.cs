d8318e8f81d869e2f44b94f3d3526e5fd33ac1b1
Tony Powell <adpowell@mtu.edu>
2019-01-22 19:11:45 -0500

    Spawn bullet on fire

--- Assets/Scripts/Weapons/Pistol/Weapon_Pistol.cs
+++ Assets/Scripts/Weapons/Pistol/Weapon_Pistol.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Weapons;
 
 public class Weapon_Pistol : Weapon {
-    
+    public GameObject bullet;
 
     private void Awake() {
         type = (int)Weapon_Constants.Weapon_types.Pistol;
@@ -12,6 +12,10 @@ public class Weapon_Pistol : Weapon {
     }
 
     protected override void Fire() {
-        
+        GameObject bullet_position = transform.Find("Bullet_Position").gameObject;
+
+        if (bullet_position != null) {
+            GameObject new_bullet = Instantiate(bullet, bullet_position.transform.position, bullet_position.transform.rotation);
+        }
     }
 }
